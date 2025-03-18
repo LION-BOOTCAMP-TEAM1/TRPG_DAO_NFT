f39f9ef674d5a63a6587c94de8caa73843cad72e
dinnerjin <sio015@naver.com>
2025-03-18 16:19:08 +0900

    34

--- .next/types/app/page.ts
+++ .next/types/app/page.ts
@@ -1,4 +1,4 @@
-// File: /Users/kiyeol/development/bootcamp/final-project/TRPG_DAO_NFT/src/app/page.tsx
+// File: C:\Users\chain\Desktop\Blockchain\Project\bcs\study\250318 TRPG\TRPG_DAO_NFT\src\app\page.tsx
 import * as entry from '../../../src/app/page.js'
 import type { ResolvingMetadata, ResolvingViewport } from 'next/dist/lib/metadata/types/metadata-interface.js'
 
