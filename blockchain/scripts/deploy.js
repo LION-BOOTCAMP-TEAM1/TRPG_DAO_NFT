67035d4f03763e5a5e65fef3d865adf54e71d0af
jkpark125 <jkpark125@gmail.com>
2025-03-19 17:02:12 +0900

    Add GameItem NFT contract

--- blockchain/scripts/deploy.js
+++ blockchain/scripts/deploy.js
@@ -6,12 +6,15 @@ async function main() {
     console.log(`Deploying contract with account: ${deployer.address}`);
 
     // 컨트랙트 배포
-    const NTF = await ethers.getContractFactory("Test");
+    const NTF = await ethers.getContractFactory("GameItems");
     const con = await NTF.deploy();
 
     await con.waitForDeployment();
-    const contractAddress = await con.getAddress();
-    console.log(`✅ deployed to: ${contractAddress}`);
+    await con.mint(deployer.address, 1, 100, "");
+    await con.mint(deployer.address, 3, 200, "");
+    await con.mint(deployer.address, 5, 300, "");
+    const itemIDs = await con.getOwnedTokens(deployer.address);
+    console.log(`✅ itemIDs: ${itemIDs}`);
 }
 
 main().catch((error) => {
