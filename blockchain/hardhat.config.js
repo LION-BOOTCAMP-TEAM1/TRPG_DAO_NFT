881c163e71fdbfe884f5a9effa52a7aa46dfc8ba
jkpark125 <jkpark125@gmail.com>
2025-03-24 14:00:23 +0900

    dependency 제거

--- blockchain/hardhat.config.js
+++ blockchain/hardhat.config.js
@@ -4,9 +4,9 @@ require("@nomicfoundation/hardhat-toolbox");
 module.exports = {
   solidity: "0.8.28",
   paths: {
-    sources: "./blockchain",
-    cache: "./blockchain/cache",
-    artifacts: "./blockchain/artifacts"
+    sources: "./",
+    cache: "./cache",
+    artifacts: "./artifacts"
   },
   networks: {
     hardhat: {}, // 로컬 네트워크
