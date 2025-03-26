975ad2f093efcba38f914238b8a258602cf14cb8
jkpark125 <jkpark125@gmail.com>
2025-03-26 16:10:49 +0900

    Add Tab component

--- frontend/src/app/equipment/EquipmentComponent.tsx
+++ frontend/src/app/equipment/EquipmentComponent.tsx
@@ -1,5 +1,7 @@
 'use client';
 
+import TabComponent from "./TabComponent";
+
 
 interface EquipmentComponentProps {
   open: boolean;
@@ -10,14 +12,14 @@ const EquipmentComponent = ({ open, onClose }: EquipmentComponentProps) => {
   return (
     <div
       className={`equipment-wrapper ${open ? 'slide-up' : 'slide-down'}
-        h-full aspect-[2/3] p-10 absolute left-0 bottom-0 shadow-lg z-10 
+        h-full aspect-[2/3] p-10 absolute shadow-lg 
         bg-[url('/equipment.png')] bg-cover bg-center
         flex justify-center items-start
       `}
     >
       <div className='flex flex-col justify-start'>
         {/* 닫기 버튼 */}
-        <div className='w-full flex flex-row justify-end m-4'>
+        <div className='w-full flex flex-row justify-end mb-2'>
           <button
             onClick={onClose}
             className="text-white text-xl bg-black/50 rounded-full w-8 h-8 flex items-center justify-center hover:bg-black/70 transition"
@@ -34,20 +36,58 @@ const EquipmentComponent = ({ open, onClose }: EquipmentComponentProps) => {
           <img src="/slot4.png" className="w-[22%] h-auto" />
         </div>
 
-        {/* 스크롤 가능한 슬롯 영역 */}
-        <div className="w-full h-full overflow-y-auto scrollbar-thin scrollbar-thumb-gray-400 scrollbar-track-transparent pr-2">
-          {/* 소유 아이템 */}
-          <div className="grid grid-cols-5 w-full justify-items-center gap-y-4">
-            {Array.from({ length: 19 }).map((_, index) => (
-              <img 
-                key={index}
-                src={`https://violet-eligible-junglefowl-936.mypinata.cloud/ipfs/bafybeiduz2r7r7y5dzkxw4kkgzhek2lty3rgoaey5fnvmblb6gsryiu6pe/${index+1}.png`}
-                className="w-[80%] h-auto" 
-              />
-            ))}
+        {/* 능력치 */}
+
+
+        {/* 내 아이템 및 NFT */}
+        <TabComponent 
+          tabs={['무기', '방어구', '칭호']}
+          contents={[
+            <div key="1">
+              <div className="w-full h-full overflow-y-auto scrollbar-thin scrollbar-thumb-gray-400 scrollbar-track-transparent pr-2">
+                <div className="grid grid-cols-5 w-full justify-items-center gap-y-4">
+                  {Array.from({ length: 10 }).map((_, index) => (
+                    <img 
+                      key={index}
+                      src={`https://violet-eligible-junglefowl-936.mypinata.cloud/ipfs/bafybeiduz2r7r7y5dzkxw4kkgzhek2lty3rgoaey5fnvmblb6gsryiu6pe/${index+1}.png`}
+                      className="w-[80%] h-auto" 
+                    />
+                  ))}
+                </div>
+              </div>
+            </div>,
+            <div key="2">
+              <div className="w-full h-full overflow-y-auto scrollbar-thin scrollbar-thumb-gray-400 scrollbar-track-transparent pr-2">
+                <div className="grid grid-cols-5 w-full justify-items-center gap-y-4">
+                  {Array.from({ length: 4 }).map((_, index) => (
+                    <div className="">
+                      <div className="absolute">asd</div>
+                      <img 
+                        key={index}
+                        src={`https://violet-eligible-junglefowl-936.mypinata.cloud/ipfs/bafybeiduz2r7r7y5dzkxw4kkgzhek2lty3rgoaey5fnvmblb6gsryiu6pe/${index+11}.png`}
+                        className="w-[80%] h-auto" 
+                      />
+                    </div>
+                  ))}
+                </div>
+              </div>
+            </div>,
+            <div key="3">
+            <div className="w-full h-full overflow-y-auto scrollbar-thin scrollbar-thumb-gray-400 scrollbar-track-transparent pr-2">
+              <div className="grid grid-cols-5 w-full justify-items-center gap-y-4">
+                {Array.from({ length: 5 }).map((_, index) => (
+                  <img 
+                    key={index}
+                    src={`https://violet-eligible-junglefowl-936.mypinata.cloud/ipfs/bafybeiduz2r7r7y5dzkxw4kkgzhek2lty3rgoaey5fnvmblb6gsryiu6pe/${index+15}.png`}
+                    className="w-[80%] h-auto" 
+                  />
+                ))}
+              </div>
+            </div>
           </div>
-        </div>
-      </div>        
+          ]}
+        />
+      </div>
     </div>
   );
 };
