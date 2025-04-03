32615e6cefa8d05f52e9d8e208848ebf388ead28
jkpark125 <jkpark125@gmail.com>
2025-04-03 15:39:24 +0900

    수량, rarity 표시

--- frontend/src/app/components/characterStat/TabComponent.tsx
+++ frontend/src/app/components/characterStat/TabComponent.tsx
@@ -33,14 +33,6 @@ const TabComponent = ({ tabs, contents, defaultIndex = 0 }: TabComponentProps) =
       {/* 콘텐츠 영역 */}
       <div className="bg-[#413029] rounded-b-md shadow-inner">
         {contents[activeIndex]}
-        <div className='w-full flex justify-center items-center'>
-          <div
-            className="flex flex-col justify-center items-center px-4 py-2 bg-gradient-to-r from-yellow-400 to-red-500 text-white rounded-xl shadow-lg hover:scale-105 hover:brightness-110 active:scale-95 transition-all duration-300 mb-4"
-          >
-            <p className='font-bold text-lg'>🎁 아이템 뽑기!</p>
-            <div className='font-light text-sm blinking-box'>단돈 0.01 🇪🇹🇭</div>
-          </div>
-        </div>
       </div>
     </div>
   );
